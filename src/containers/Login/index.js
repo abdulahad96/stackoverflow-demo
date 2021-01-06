9b121af667861e4a597326794a22b87d641d836c
AbdulAhad <35425896+abdulahad96@users.noreply.github.com>
2021-01-06 12:08:04 +0500

    login sign up

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,119 +1,61 @@
-import React, { Component,useState } from "react";
+import React, { Component, useState } from "react";
 import { DotsLoader, Navbar } from "../../components";
 import { Images } from "../../theme/index";
+import Logo from '../../assets/images/logo.png'
 import "./styles.css";
 const Login = ({ }) => {
 
-const [emailError,setEmailError]= useState(false)
-const [passwordError,setPasswordError]= useState(false)
-const [password,setPassword] = useState(null); 
-const [email,setEmail] = useState(null);
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(true)
+    const [password, setPassword] = useState(null);
+    const [email, setEmail] = useState(null);
     return (
         <>
-            <div className="container-fluid">
-                <div className="container">
-                    <Navbar isBackgroundColor={true} />
-                </div>
-                <div className="container" style={{ backgroundColor: "#F8FDFF" }}>
-                    <div className="row mt-5">
-                        <div className="col-sm-9 col-md-7 col-lg-5 mx-auto mt-5">
-                        <div className="card card-signin my-5">
-                <div className="card-body px-0">
-                    <h5 className="card-title text-center">Sign in</h5>
-                    <div
-                        className="row px-3"
-                        style={{ alignItems: "center", justifyContent: "center" }}
-                    >
-                        <span className=" mb-2 google-btn">
-                            <img className="ml-0 google-btn-img" src={Images.google_signin} />
-                            <span className="google-btn-text " onClick={() => { }} >Sign In With Google</span>
-                        </span>
-                    </div>
-                    <div
-                        className="row px-3"
-                        style={{ justifyContent: "center", alignItems: "center" }}
-                    >
-                        <span className=" mb-2 facebook-btn">
-                            <img className="facebook-btn-img" src={Images.facebook_signin} />
-                            <span className="facebook-btn-text"  >Facebook</span>
-                        </span>
-                    </div>
-                    <div
-                        className="row px-3"
-                        style={{ justifyContent: "center", alignItems: "center" }}
-                    >
-                        <span className="  py-1 py-md-2 py-lg-2  email-btn">
-                            <img className="email-btn-img" src={Images.mail_signin} />
-                            <span className="email-btn-text">Email</span>
-                        </span>
-                    </div>
-                    <div className="row">
-                        <hr className="my-4 mx-3 " style={{ width: "100%" }} />
-                    </div>
+            <div class="container">
+                <img id="logo" src={Logo} />
 
-                    <form action="javascript:void(0)" className="form-signin  px-4">
+                <form id="signup">
+
+                    <div class="header">
+
+                        <h3>Sign In</h3>
 
 
-                        <div className="form-label-group mb-4 ">
-                            <label for="inputemail">Email</label>
-                            <input
-                                type="text"
-                                id="inputemail"
-                                className="form-control mt-1"
-                                placeholder="Enter your Email..."
-                                onChange={(text) => setEmail(text)}
-                                value={email}
-                            />
-                            {emailError ? (
-                                <small className="form-text text-danger ml-3">
-                                    {emailError}
-                                </small>
-                            ) : null}
-                        </div>
-
-                        <div className="form-label-group">
-                            <label for="inputPassword ">Password</label>
-                            <input
-                                type="password"
-                                id="inputPassword"
-                                className="form-control mt-1"
-                                placeholder="Enter your password..."
-                                onChange={(text) => setPassword(text)}
-                                value={password}
-                            />
-                            {passwordError ? (
-                                <small className="form-text text-danger ml-3">
-                                    {passwordError}
-                                </small>
-                            ) : null}
-                        </div>
-
-                        <div className="form-signin  px-8">
-                            <span
-                                style={{
-                                    color: "#ff3600",
-                                    fontWeight: "700",
-                                    cursor: "pointer",
-                                    marginBottom: 20,
-                                }}
-                                onClick={() => {}}
-                            >
-                                Forget Password
-                        </span>
-                        </div>
-                        <button
-                            className="login-btn btn btn-lg"
-                            type="submit"
-                            onClick={() => {}}
-                        >
-                            Login Now
-                        </button>
-                    </form>
-                </div>
-            </div>
-                        </div>
                     </div>
-                </div>
+
+                    <div class="sep"></div>
+
+                    <div class="inputs">
+
+                        <input type="email" onChange={text => setEmail(text.target.value)} placeholder="e-mail" autofocus />
+
+                        {emailError && <p className='error'>Please Enter valid Email</p>}
+                        <input type="password" onChange={text => setPassword(text.target.value)} placeholder="Password" />
+
+                        {passwordError && <p className='error'>Please Enter atleast 8 characters</p>}
+
+                        <button id="submit" onClick={() => {
+                            const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+                            console.log(password,!password,password.length)
+                            if (!email || !email.match(emailRegex)) {
+                                setEmailError(true)
+                                setTimeout(() => {
+                                    setEmailError(false)
+                                }, 1500);
+                            }
+                            else if (!password || !password.length  >= 8) {
+                                setPasswordError(true)
+                                setTimeout(() => {
+                                    setPasswordError(false)
+                                }, 1500);
+                            }
+                        }}>Login</button>
+
+                    </div>
+
+                </form>
+
             </div>
         </>
     )
